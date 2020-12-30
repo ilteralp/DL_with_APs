ea98249a0f73aa7ff28ff5514358ff3cc3701350
melike <milteralp@gmail.com>
2020-12-30 20:01:01 +0300

    Instead of using constant height and width, they are calculated from given patch (since patch_size is also a param in the dataset). Layers and their params are added as dict.

--- model.py
+++ model.py
@@ -12,8 +12,9 @@ Resources:
 import torch
 import torch.nn as nn
 
-K1, K2, K3 = 5, 3, 3                                                            # Kernel sizes of corresponding conv layers.
-
+conv1 = {'ks': 5, 'pad': 2, 'st': 1}                                            # Kernel sizes are from the article. Padding and stride are set in order to make output shape same as input shape. 
+conv2 = {'ks': 3, 'pad': 1, 'st': 1}
+conv3 = {'ks': 3, 'pad': 1, 'st': 1}
 
 class APNet(nn.Module):
     r""" 
@@ -21,28 +22,32 @@ class APNet(nn.Module):
     Hyperspectral Image Classification paper. 
     """
     
-    def __init__(self, in_channels, num_classes, L):
+    def __init__(self, in_channels, L, H, W, num_classes):
         """
         Args:
             in_channels (int): Number of channels of a sample patch. 
+            L (int): Number of AP images (length of AP thresholds + PC image of that channel) or 1 for spectral signature for a sample. 
+            H (int): Height of a sample patch. 
+            W (int): Width of a sample patch. 
             num_classes (int): Number of classes within the dataset. 
-            L (int): Number of AP images (length of AP thresholds + PC image of that channel).
         """
         super(APNet, self).__init__()
         self.in_channels = in_channels
+        self.last_conv_d = self.calc_last_conv(L)                               # L (number of thresholds of AP) varies, so calculate the last conv layer's depth.
+        self.last_conv_h = self.calc_last_conv(H)
+        self.last_conv_w = self.calc_last_conv(W)
         self.num_classes = num_classes
-        self.convd_depth = L - (K1 - 1 + K2 - 1 + K3 - 1)                       # L (number of thresholds of AP) varies, so calculate convd depth.
         self.features = nn.Sequential(
-            nn.Conv3d(self.in_channels, 48, kernel_size=K1),
+            nn.Conv3d(self.in_channels, 48, kernel_size=conv1['ks'], padding=conv1['pad'], stride=conv1['st']),
             nn.ReLU(inplace=True),
-            nn.Conv3d(48, 96, kernel_size=K2),
+            nn.Conv3d(48, 96, kernel_size=conv2['ks'], padding=conv2['pad'], stride=conv2['st']),
             nn.ReLU(inplace=True),
-            nn.Conv3d(96, 96, kernel_size=K3),
+            nn.Conv3d(96, 96, kernel_size=conv3['ks'], padding=conv3['pad'], stride=conv3['st']),
             nn.ReLU(inplace=True)
         )
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.5),
-            nn.Linear(96 * self.convd_depth, 1024),                             # convd_depth varies but convd_h and convd_w are constant, being 9-(5-1+3-1+3-1)=1
+            nn.Linear(96 * self.last_conv_d * self.last_conv_h * self.last_conv_w, 1024),    # input to the 1st linear should be num_filters * depth * h * w.
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.5),
             nn.Linear(1024, 1024),
@@ -55,3 +60,13 @@ class APNet(nn.Module):
         x = torch.flatten(x, 1)                                                 # Flatten starts from 1st dim, leave batch_size dim.
         x = self.classifier(x)                                                  # No softmax due to using cross entropy loss.
         return x
+    
+    """
+    Takes an input shape, returns output shape of it after convolutions. See 
+    https://pytorch.org/docs/stable/generated/torch.nn.Conv3d.html
+    """
+    def calc_last_conv(self, shape):
+        dil  = 1                                                                # Default value of dilation in PyTorch.
+        for layer in [conv1, conv2, conv3]:
+            shape = int((shape + 2 * layer['pad'] - dil * (layer['ks'] - 1) - 1) / layer['st']) + 1
+        return shape 
